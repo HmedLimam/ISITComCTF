865a6dde542106d499c67fe7940c59b19dad8020
infreezy <57916366+infreezy@users.noreply.github.com>
2021-03-03 00:23:50 +0100

    Update Mr_A.py

--- Crypto/Mr_A.py
+++ Crypto/Mr_A.py
@@ -1,6 +1,6 @@
 # Title: Mr.A
 # Hint: Xor might be useful
-# The idea of this challenge is to use the XOR operator in each of the numbers with ord(A)
+# The idea of this challenge is to use the XOR operator in each of the numbers with ord(A) and return it into text with chr()
 
 text = """14, 36, 42, 97, 53, 56, 53, 97, 40, 36, 97, 44, 52, 35, 35, 96, 97, 54, 
 36, 36, 53, 97, 59, 36, 51, 96, 97, 56, 37, 55, 58, 11, 25, 16, 11, 102, 8, 
